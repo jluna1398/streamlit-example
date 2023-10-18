9713325d9d21d18c2817470b4f8e7fe0b7b843c4
jluna1398 <60058404+jluna1398@users.noreply.github.com>
2023-10-17 19:49:47 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
-import scikit-learn
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+from sklearn.ensemble import RandomForestClassifier
 
 
 with st.echo(code_location='below'):
